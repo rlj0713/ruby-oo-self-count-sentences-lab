cb2627f92e369ff400f1841a059352c4e2487cc2
Reid <rlj0713@gmail.com>
2021-01-24 13:52:45 -0500

    commit tree bbcbaf41de9f205a0d1a60f3cb21493d742f6e9d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
-
 class String
 
+  def initialize(name)
+    @name = name
+  end
+
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    new = self.split(/(!)+|(\?)+|(\.)+\s|\z/)
+    (new.length + 1) / 2
   end
+
 end
